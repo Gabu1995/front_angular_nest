2ed6beec1e260c46e28db67d0ab4283af5a6901a
Gabu1995 <gabrielcajas1995@hotmail.com>
2024-05-31 09:03:48 -0500

    clase 31/05/2024
    clase 13 conectar back end y generrar token

--- src/app/auth/components/login/login.component.ts
+++ src/app/auth/components/login/login.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { error } from 'console';
 
 @Component({
   selector: 'app-login',
@@ -7,4 +10,22 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  private http=inject(HttpClient)
+
+  loginForm=new FormGroup({
+    email:new FormControl("", [Validators.email, Validators.required]),
+    password:new FormControl("", Validators.required)
+  })
+
+    funIngresar(){
+      this.http.get("http://jsonplaceholder.typicode.com/todos").subscribe(
+        (res)=>{
+          console.log(res)
+        },
+        (error)=>{
+          console.log(error)
+        }
+      )
+      //alert("Ingresando...")
+    }
 }
